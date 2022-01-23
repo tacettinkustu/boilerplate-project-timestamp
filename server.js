7481e8a01fe8aac6b31f1705d6265c994a3d4158
Tacettin Kustu <tacettinkustu@gmail.com>
2022-01-23 18:22:23 +0300

    Validate for date format

--- server.js
+++ server.js
@@ -4,30 +4,38 @@
 // init project
 var express = require('express');
 var app = express();
+const moment = require('moment');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
-let resObj = {}
+let resObj = {};
 // endpoint for date
-app.get("/api/:date", function (req, res) {
-  let input = req.params.date;
+app.get('/api/:date', function (req, res) {
+  let inputDate = req.params.date;
+  if (moment(inputDate, 'YYYY-MM-DD', true).isValid()) {
+    res.json({ success: 'true' });
+  } else if (moment(inputDate).isValid()) {
+    //unix format
+    res.json({ successUnix: 'true' });
+  } else {
+    res.json({ error: 'Invalid Date' });
+  }
   res.json(input);
 });
 
